a8c471d5072b89818e6a494d0b286c79134143b4
MarcelaCCR <marcela.rezende21@gmail.com>
2025-06-03 22:46:41 -0300

    implementado exibição de detalhes da tarefa

--- src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
+++ src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
@@ -10,4 +10,8 @@ public class Tarefa {
         this.descricao = descricao;
         this.concluida = concluida;
     }
+
+    public String obterDados(){
+        return String.format("ID: %d | Descrição: %s| Status: %s",this.id,this.descricao,this.concluida ? "Concluida" : "Incompleta");
+    }
 }
